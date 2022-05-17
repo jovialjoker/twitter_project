e9d2b261f8cd2d90e7e974ee6ed97571dd1ff0b4
Florea George <florgeo_02@yahoo.com>
2022-05-17 15:02:26 +0300

    working login, register and index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 3000;
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({extended: true}))
 // Setting the EJS for template rendering
@@ -11,7 +12,57 @@ app.set('views', path.join(__dirname, '../client/views'));
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.get('/', (_req, res) => {
-    res.render('index', { message: 'Error message' });
+    fs.readFile(path.join(path.dirname(__dirname), 'server', 'mock_data', 'postari.json'), 'utf8', ((err, data) => {
+        if (err) {
+            return res.render('index', {message: 'Posts are not available'})
+        }
+        data = JSON.parse(data);
+        return res.render('index', {postari: data});
+    }));
+})
+
+app.post('/', (req, res) => {
+    const fisier = fs.readFileSync(path.join(path.dirname(__dirname), 'server', 'mock_data', 'postari.json'));
+    const postari = JSON.parse(fisier);
+    postari.push(req.body);
+    const postariNoi = JSON.stringify(postari);
+    fs.writeFileSync(path.join(path.dirname(__dirname), 'server', 'mock_data', 'postari.json'), postariNoi, err => {
+        if(err) {
+            console.log(err);
+        }
+    })
+})
+
+app.get('/login', (_req, res) => {
+    fs.readFile(path.join(path.dirname(__dirname), 'server', 'mock_data', 'users.json'), 'utf8', ((err, data) => {
+        if (err) {
+            return res.render('login', {message: 'Posts are not available'})
+        }
+        data = JSON.parse(data);
+        return res.render('login', {users: data});
+    }));
+})
+
+app.get('/register', (_req, res) => {
+    fs.readFile(path.join(path.dirname(__dirname), 'server', 'mock_data', 'users.json'), 'utf8', ((err, data) => {
+        if (err) { 
+            return res.render('register', {message: 'Posts are not available'})
+        }
+        data = JSON.parse(data);
+        return res.render('register', {users: data});
+    }));
+})
+
+app.post('/register', (req, res) => {
+    const fisier = fs.readFileSync(path.join(path.dirname(__dirname), 'server', 'mock_data', 'users.json'));
+    const users = JSON.parse(fisier);
+    users.push(req.body);
+    const new_users = JSON.stringify(users);
+    fs.writeFileSync(path.join(path.dirname(__dirname), 'server', 'mock_data', 'users.json'), new_users, err => {
+        if(err) {
+            console.log(err);
+        }
+    });
 })
 
 app.listen(PORT, () => {
